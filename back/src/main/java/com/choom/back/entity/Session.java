e34ec56daa8cac4b32048fd8427e7776e0a34abb
Lavorary <hei.lavorary@gmail.com>
2026-05-01 21:20:17 +0300

    feat : create findALlSession functionalities in SessionRepository + rework entity Session

--- back/src/main/java/com/choom/back/entity/Session.java
+++ back/src/main/java/com/choom/back/entity/Session.java
@@ -1,19 +1,17 @@
-package com.choom.back.model;
+package com.choom.back.entity;
 
 import lombok.Data;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-@Entity
 @Data
 public class Session {
-    @Id
-    private long eventId;
+    private UUID eventId;
     private UUID SessionId;
     private String title;
     private String description;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
-    private Room room;
+    private UUID room;
 }
---
+++ back/src/main/java/com/choom/back/repository/SessionRepository.java
@@ -0,0 +1,49 @@
+package com.choom.back.repository;
+
+import com.choom.back.config.DBConfig;
+import com.choom.back.entity.Session;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+@AllArgsConstructor
+public class SessionRepository {
+
+    private DBConfig dbConfig;
+    public List<Session> findAllSession() {
+        List<Session> sessionList = new ArrayList<>();
+        String query = """
+                SELECT id, title, description, start_time, end_time, room_id, capacity, event_id
+                FROM session
+                ORDER BY start_time; 
+                """;
+
+        try(Connection connection = dbConfig.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(query)){
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()){
+                Session session = new Session();
+                session.setSessionId((UUID) resultSet.getObject("id"));
+                session.setTitle(resultSet.getString("title"));
+                session.setDescription(resultSet.getString("description"));
+                session.setStartTime(resultSet.getTimestamp("start_time").toLocalDateTime());
+                session.setEndTime(resultSet.getTimestamp("end_time").toLocalDateTime());
+                session.setRoom((UUID) resultSet.getObject("room_id"));
+                session.setEventId((UUID) resultSet.getObject("event_id"));
+                sessionList.add(session);
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+        return sessionList;
+    }
+}
