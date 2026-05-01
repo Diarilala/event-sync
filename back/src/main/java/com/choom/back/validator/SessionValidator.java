47f37307d8144c4b7744521a958947fee7fdd5fe
Lavorary <hei.lavorary@gmail.com>
2026-05-01 23:26:45 +0300

    feat : Validate Update in Validator

--- back/src/main/java/com/choom/back/validator/SessionValidator.java
+++ back/src/main/java/com/choom/back/validator/SessionValidator.java
@@ -5,6 +5,8 @@ import com.choom.back.repository.SessionRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Component;
 
+import java.util.UUID;
+
 @Component
 @AllArgsConstructor
 
@@ -49,6 +51,65 @@ public class SessionValidator {
         if(!message.isEmpty()) {
             throw new RuntimeException(message);
         }
+    }
+
+    public void ExistingValidate(UUID id) {
+        String message = "";
+
+        if(id == null) {
+            message += "Session ID is required";
+        }
+
+
+
+        if(!message.isEmpty()) {
+            throw new RuntimeException(message);
+        }
+
+        if(id != null && !sessionRepository.existsSessionById(id)) {
+            message += "Session with id: "+ id + " does not exist";
+        }
+    }
+
+    public void UpdateValidate(UUID id, Session session) {
+        String message = "";
+        if(id == null) {
+            message += "Session ID is required";
+        }
+
+        if(session == null) {
+            message += "Session is null";
+        }
+
+        if(session.getEventId() == null) {
+            message += "Event ID is required";
+        }
+
+        if(session.getTitle() == null || session.getTitle().isBlank()) {
+            message += "Title is required";
+        }
+
+        if(session.getDescription() == null || session.getDescription().isBlank()) {
+            message += "Description is required";
+        }
+
+        if(session.getStartTime().isAfter(session.getEndTime())) {
+            message += "Start time is after end time";
+        }
+
+        if(session.getRoom() == null || !session.getRoom().equals(session.getRoom())) {
+            message += "Room number is incorrect or the room doesn't exist";
+        }
+
+        if(session.getSessionId() != null && sessionRepository.existsSessionById(session.getSessionId())) {
+            message += "Session with id: "+ session.getSessionId() + " already exists";
+        }
+
+        if(session.getDescription() == null || session.getDescription().isBlank()) {
+            message += "Description is required";
+        }
+
+
     }
 
 }
