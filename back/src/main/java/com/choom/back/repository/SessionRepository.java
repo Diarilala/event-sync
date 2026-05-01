cbab79a546c28ed85c53bba4ef73b9918935d32f
Lavorary <hei.lavorary@gmail.com>
2026-05-01 21:37:29 +0300

    feat : create findSessionById function in SessionRepository

--- back/src/main/java/com/choom/back/repository/SessionRepository.java
+++ back/src/main/java/com/choom/back/repository/SessionRepository.java
@@ -47,4 +47,34 @@ public class SessionRepository {
 
         return sessionList;
     }
+
+    public Session findSessionById(UUID id) {
+        String  query = """
+                SELECT id, title, description, start_time, end_time, room_id, capacity, event_id
+                FROM session
+                WHERE id = ?;
+        """;
+
+        try(Connection connection = dbConfig.getConnection();
+        PreparedStatement preparedStatement = connection.prepareStatement(query)){
+            preparedStatement.setObject(1, id);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            while(resultSet.next()){
+                Session session = new Session();
+                session.setSessionId((UUID) resultSet.getObject("id"));
+                session.setTitle(resultSet.getString("title"));
+                session.setDescription(resultSet.getString("description"));
+                session.setStartTime(resultSet.getTimestamp("start_time").toLocalDateTime());
+                session.setEndTime(resultSet.getTimestamp("end_time").toLocalDateTime());
+                session.setRoom((UUID) resultSet.getObject("room_id"));
+                session.setEventId((UUID) resultSet.getObject("event_id"));
+                return session;
+            }
+    }
+        catch (SQLException e) {
+        throw new RuntimeException(e);
+        }
+        return null;
+    }
+
 }
