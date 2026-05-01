ab49aa6ca97f6c29bc88583eecbe31ccc00ddabe
Lavorary <hei.lavorary@gmail.com>
2026-05-01 23:42:34 +0300

    feat : update and create in SessionService

--- back/src/main/java/com/choom/back/service/SessionService.java
+++ back/src/main/java/com/choom/back/service/SessionService.java
@@ -1,8 +1,10 @@
 package com.choom.back.service;
 
 import com.choom.back.entity.Session;
+import com.choom.back.exception.BadRequestException;
 import com.choom.back.exception.NotFoundException;
 import com.choom.back.repository.SessionRepository;
+import com.choom.back.validator.SessionValidator;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -13,6 +15,7 @@ import java.util.UUID;
 @AllArgsConstructor
 public class SessionService {
     private final SessionRepository sessionRepository;
+    private final SessionValidator sessionValidator;
 
     public List<Session> getAllSession(){
         return sessionRepository.findAllSession();
@@ -33,4 +36,31 @@ public class SessionService {
         }
         return sessions;
     }
+
+    public Session createSession(Session session){
+        sessionValidator.validate(session);
+
+        Session created = sessionRepository.createSession(session);
+        if(created == null){
+            throw new BadRequestException("Failed to create session");
+        }
+        return session;
+    }
+
+    public Session updateSession(UUID id, Session session){
+        sessionValidator.UpdateValidate(id, session);
+        session.setSessionId(id);
+
+        Session updated = sessionRepository.updateSession(session);
+        if(updated == null){
+           throw new BadRequestException("Failed to update session");
+        }
+        return session;
+    }
+    public void deleteSession(UUID id){
+        sessionRepository.deleteSessionById(id);
+
+        throw new RuntimeException("Not implemented yet");
+        }
+
 }
