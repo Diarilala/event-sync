f0335a069fb788ec1f75201ce0639dcbe7feb947
Lavorary <hei.lavorary@gmail.com>
2026-05-01 22:28:13 +0300

    feat : create getSessionById in SessionService

--- back/src/main/java/com/choom/back/service/SessionService.java
+++ back/src/main/java/com/choom/back/service/SessionService.java
@@ -1,11 +1,13 @@
 package com.choom.back.service;
 
 import com.choom.back.entity.Session;
+import com.choom.back.exception.NotFoundException;
 import com.choom.back.repository.SessionRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 @AllArgsConstructor
@@ -14,5 +16,13 @@ public class SessionService {
 
     public List<Session> getAllSession(){
         return sessionRepository.findAllSession();
+    }
+
+    public Session getSessionById(UUID id){
+        try {
+            return sessionRepository.findSessionById(id);
+        } catch (NotFoundException e){
+            throw new NotFoundException("Session with id " + id + " not found");
+        }
     }
 }
