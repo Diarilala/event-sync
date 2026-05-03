6f3f27a2f52a047a0eafadf9c98369fbd942d8b0
N0mena <alida.rajaonarivelo@gmail.com>
2026-05-03 19:50:45 +0300

    fix: string id to UUID id

--- back/src/main/java/com/choom/back/entity/Question.java
+++ back/src/main/java/com/choom/back/entity/Question.java
@@ -4,12 +4,13 @@ import lombok.Data;
 
 import java.sql.Timestamp;
 import java.util.List;
+import java.util.UUID;
 
 @Data
 public class Question {
-    private String id;
+    private UUID id;
     private String content;
-    private String name;
+    private String auhtorName;
     private Timestamp creationDate;
     private List<Answer> answers;
     private Integer upvoteCount;
