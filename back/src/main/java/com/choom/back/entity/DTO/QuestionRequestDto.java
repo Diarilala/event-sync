f42d918d8eb4639362dc11d49d4b4aed0471b1ee
N0mena <alida.rajaonarivelo@gmail.com>
2026-05-03 19:49:00 +0300

    feat: add question repository

--- back/src/main/java/com/choom/back/entity/DTO/QuestionRequestDto.java
+++
@@ -1,8 +0,0 @@
-package com.choom.back.entity.DTO;
-
-import lombok.Data;
-
-@Data
-public class QuestionRequestDto {
-
-}
--- back/src/main/java/com/choom/back/repository/QuestionRepository.java
+++ back/src/main/java/com/choom/back/repository/QuestionRepository.java
@@ -1,7 +1,55 @@
 package com.choom.back.repository;
 
+import com.choom.back.config.DBConfig;
+import com.choom.back.entity.Question;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+
+@AllArgsConstructor
 @Repository
 public class QuestionRepository {
+    private final DBConfig dbConfig;
+
+    public Question savaQuestion(Question question){
+
+        String saveQuestionQuery = "Insert into question (id, content,author_name, creation_date, upvote_count) " +
+                "values (?, ? , ?, ?) ";
+
+        try(Connection connection = dbConfig.getConnection();
+            PreparedStatement preparedStatement = connection.prepareStatement(saveQuestionQuery)){
+
+            if(question.getId() == null){
+                question.setId(UUID.randomUUID());
+            }
+
+            preparedStatement.setObject(1, question.getId());
+            preparedStatement.setString(2, question.getContent());
+            preparedStatement.setString(3, question.getAuhtorName());
+
+            Timestamp creationDate = question.getCreationDate() != null
+                    ? question.getCreationDate()
+                    : new Timestamp(System.currentTimeMillis());
+            preparedStatement.setTimestamp(4, creationDate);
+
+            preparedStatement.setInt(5, question.getUpvoteCount() != null
+                    ? question.getUpvoteCount()
+                    : 0);
+
+            preparedStatement.executeUpdate();
+            return question;
+
+        } catch (SQLException e) {
+            throw new RuntimeException("error saving question",e);
+        }
+    }
+
 }
